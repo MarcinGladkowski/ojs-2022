c18d11d8100937ff2c94eb1d87e8cddc5ee958a3
MarcinGladkowski <marcingladkowski@gmail.com>
2022-03-14 20:13:35 +0100

    bind function

--- 1/index.js
+++ 1/index.js
@@ -1,5 +1,25 @@
+const Mailer = require('./mailer.js');
+const Notification = require('./push-notification');
+const SmsService = require('./sms-service');
+
 function send(message) {
     this.sendMessage(message);
 }
 
-send('Hello world!');
+/**
+ * 3 different type of codes and using context using bind() function
+ * - allows connecting legacy code
+ */
+
+const callMailer = send.bind(new Mailer());
+callMailer('Hello from Mailer')
+
+const callNotification = send.bind(new Notification());
+callNotification('Hello from Notification')
+
+const callSmsService = send.bind(SmsService);
+callSmsService("Hello from SMSService")
+
+
+
+
