d90f22b529d42d5b45e7f777ff31d07d107b6b3b
MarcinGladkowski <marcingladkowski@gmail.com>
2022-03-15 14:01:12 +0100

    wrap details as Promise

--- 5/index.js
+++ 5/index.js
@@ -13,23 +13,6 @@ function fetchData(params, onSuccess, onError) {
     req.send(null);
 }
 
-/**
- *
- * Return promise
- *
- * @param userId
- * @param onSuccess
- * @param onError
- * @returns {Promise<void>}
- */
-function fetchProfile(userId, onSuccess, onError) {
-    fetchData({ method: 'GET', url: `http://localhost:8080/app/profile/${userId}`}, onSuccess, onError)
-}
-
-async function fetchPaymentDetails(userId, onSuccess, onError) {
-    fetchData({ method: 'GET', url: `http://localhost:8080/app/payments/${userId}`}, onSuccess, onError)
-}
-
 let userProfile = null
 const userId = 4
 
@@ -42,27 +25,24 @@ const fetchPromise = new Promise((resolve, reject) => {
         })
 })
 
-const fetchDetails = new Promise((resolve, reject) => {
-    fetchData({ method: 'GET', url: `http://localhost:8080/app/payments/${userId}`},
-        (response) => {
-            resolve(response)
-        }, (error) => {
-            reject(error)
-        })
-})
+const fetchDetails = function (userId, userProfile) {
+    return new Promise((resolve, reject) => {
+        fetchData({ method: 'GET', url: `http://localhost:8080/app/payments/${userId}`},
+            (payments) => {
+                document.querySelector('#user-name').textContent = `User: ${userProfile.firstName} ${userProfile.lastName}`
+                document.querySelector('#user-subscription').textContent = `Subscription: ${payments.subscriptionStatus}`
+                resolve(payments)
+            }, (error) => {
+                window.alert('Cannot fetch payment details!')
+                reject()
+            })
+    })
+}
 
 
 fetchPromise.then((profile) => {
     userProfile = profile
-}).then(() => {
-
-    fetchPaymentDetails(userId, (payments) => {
-        document.querySelector('#user-name').textContent = `User: ${userProfile.firstName} ${userProfile.lastName}`
-        document.querySelector('#user-subscription').textContent = `Subscription: ${payments.subscriptionStatus}`
-    }, () => {
-        window.alert('Cannot fetch payment details!')
-    })
+    return userProfile
+}).then((userProfile) => {
+    fetchDetails(userId, userProfile)
 })
-
-
-
